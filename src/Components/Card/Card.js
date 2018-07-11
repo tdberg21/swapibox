548496c13cd97b13f9752fd9385bd7024cc790fb
Tory Dannenberg <rfdberg@gmail.com>
2018-07-10 21:31:06 -0600

    Move people API call to api call file

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -5,9 +5,10 @@ import './Card.css';
 
 const Card = (props) => {
 
-  let cardToRender = Object.keys(props.info).map(item => {
-    console.log(props.info[item])
-    return <p> {[item]} : {props.info[item]} </p>
+  const cardToRender = Object.keys(props.info).map((item, index) => {
+    if (item !== 'id') {
+      return <p key={index}> {[item]} : {props.info[item]} </p>;
+    }
   });
 
   return (
@@ -20,62 +21,6 @@ const Card = (props) => {
       </button>
     </div>
   );
-
-
-  // if (props.info.species) {
-  //   return (
-  //     <div className="card">
-  //       <h5>{props.info.name}</h5>
-  //       <p>Species: {props.info.species} </p>
-  //       <p>Homeworld: {props.info.homeworld}</p>
-  //       <p>Homeworld population:{props.info.population}</p>
-  //       <button 
-  //         onClick={(event) => props.checkForFaves(props.info.id, event)}
-  //         className="favorite-button">
-  //         Favorite
-  //       </button>
-  //     </div>
-  //   );
-  // } 
-  
-  // if (props.info.terrain) {
-  //   const residentsToDisplay = props.info.residents.map((resident, index) => {
-  //     return (<li key={index}>{resident}</li>);
-  //   });
-    
-  //   return (
-  //     <div className="card">
-  //       <h5>{props.info.name}</h5>
-  //       <p>Climate: {props.info.climate} </p>
-  //       <p>Terrain: {props.info.terrain}</p>
-  //       <p>Population:{props.info.population}</p>
-  //       <ul>Residents:{residentsToDisplay}</ul>
-  //       <button 
-  //         onClick={(event) => props.checkForFaves(props.info.id, event)}
-  //         className="favorite-button"
-  //       >
-  //       Favorite
-  //       </button>
-  //     </div>
-  //   );
-  // } 
-   
-  // if (props.info.model) {
-  //   return (
-  //     <div className="card">
-  //       <h5>{props.info.name}</h5>
-  //       <p>Model: {props.info.model} </p>
-  //       <p>Class: {props.info.class}</p>
-  //       <p>Passengers:{props.info.passengers}</p>
-  //       <button 
-  //         onClick={(event) => props.checkForFaves(props.info.id, event)}
-  //         className="favorite-button"
-  //       >
-  //       Favorite
-  //       </button>
-  //     </div>
-  //   );
-  // }
 };
 
 Card.propTypes = {
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,7 @@ const generateRandomNumber = () => {
   return Math.floor(Math.random() * 6) + 1;
 };
 
-const fetchData = async (category) => {
+export const fetchData = async (category) => {
   let number = generateRandomNumber();
   let url = `https://swapi.co/api/${category}/${number}/`;
   try {
@@ -15,4 +15,41 @@ const fetchData = async (category) => {
   }
 };
 
-export default fetchData;
+export const getPeopleData = async (category) => {
+  const url = `https://swapi.co/api/${category}/`;
+  try {
+    const response = await fetch(url);
+    const parsedResponse = await response.json();
+    const residentsToFetch = await fetchHomeWorld(parsedResponse.results);
+    const people = await fetchSpecies(residentsToFetch);
+    return people;
+  } catch (error) {
+    alert(error);
+  }   
+};
+
+const fetchHomeWorld = (response) => {
+  const unresolvedPromises = response.map(person => (
+    fetch(person.homeworld)
+      .then(response => response.json())
+      .then(results => ({ ...person, homeworld: results.name, population: results.population }))
+  ));
+  const species = (Promise.all(unresolvedPromises));
+  return species;
+};
+
+const fetchSpecies = async (response) => {
+  console.log(response);
+  const unresolvedPromises = await response.map((person, index) => (
+    fetch(person.species)
+      .then(response => response.json())
+      .then(results => ({
+        name: person.name,
+        homeworld: person.homeworld,
+        population: person.population,
+        species: results.name,
+        id: `${index} ${person.name}`
+      }))
+  ));
+  return Promise.all(unresolvedPromises);
+};
